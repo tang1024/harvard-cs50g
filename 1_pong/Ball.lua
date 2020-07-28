7463b64847d08f717c2ffa3d44a3cb73862a8bdd
Jian Tang <jian.tang.smtp@gmail.com>
2020-07-27 19:16:15 -0700

    pong done except AI

--- 1_pong/Ball.lua
+++ 1_pong/Ball.lua
@@ -1,18 +1,6 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    -- Ball Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Represents a ball which will bounce back and forth between paddles
-    and walls until it passes a left or right boundary of the screen,
-    scoring a point for the opponent.
-]]
-
 Ball = Class{}
+
+BALL_SPEED_X = 100
 
 function Ball:init(x, y, width, height)
     self.x = x
@@ -20,41 +8,27 @@ function Ball:init(x, y, width, height)
     self.width = width
     self.height = height
 
-    -- these variables are for keeping track of our velocity on both the
-    -- X and Y axis, since the ball can move in two dimensions
-    self.dy = 0
-    self.dx = 0
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-BALL_SPEED_X, BALL_SPEED_X)
 end
 
---[[
-    Expects a paddle as an argument and returns true or false, depending
-    on whether their rectangles overlap.
-]]
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-BALL_SPEED_X, BALL_SPEED_X)
+end
+
 function Ball:collides(paddle)
-    -- first, check to see if the left edge of either is farther to the right
-    -- than the right edge of the other
     if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
         return false
     end
 
-    -- then check to see if the bottom edge of either is higher than the top
-    -- edge of the other
     if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
         return false
     end 
 
-    -- if the above aren't true, they're overlapping
     return true
-end
-
---[[
-    Places the ball in the middle of the screen, with no movement.
-]]
-function Ball:reset()
-    self.x = VIRTUAL_WIDTH / 2 - 2
-    self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dx = 0
-    self.dy = 0
 end
 
 function Ball:update(dt)
